67cab9fca729711b8f62d6da2bd21557755f144e
Rairu <lylekevin.tamarra.21@usjr.edu.ph>
2023-04-28 14:47:04 +0800

    add backend & frontend folder

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../css/Footer.css";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer-container">
+        <h4>Created By You</h4>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
